0f6fa309ccb4575c99d6611f89ef22bf6ae45c65
Алексей Тряпко <aatryapko@mail.ru>
2018-11-28 00:29:38 +0200

    Add theme and scoped CSS styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -10,14 +11,6 @@ export default {
   name: 'app'
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'assets/css/style.css';
 </style>
